bb6e5d4efef5376828726ca7bd0f430698aa9d2f
alu20484867d <alu20484867d@ieselcaminas.org>
2019-06-02 18:58:37 +0200

    --

---
+++ src/NodoPila.java
@@ -0,0 +1,9 @@
+
+public class NodoPila {
+			public NodoPila(double dato, NodoPila abajo) {
+					this.dato = dato; 
+						this.abajo = abajo;
+			}
+			public NodoPila abajo;
+			public double dato;
+}
